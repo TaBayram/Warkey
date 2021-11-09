3c6bc412781989fedf3e2e1e2363789d38d9c985
TaBayram <48648542+TaBayram@users.noreply.github.com>
2021-11-09 12:46:49 +0300

    Separate Editor

--- Warkey/Assets/Scripts/Entity/Unit.cs
+++ Warkey/Assets/Scripts/Entity/Unit.cs
@@ -9,12 +9,9 @@ public class Unit : MonoBehaviour,IWidget
     private FiniteField health;
     private FiniteField stamina;
 
-    public WeaponController weaponController;
-    public Movement movement;
+
 
     private void Start() {
-        weaponController = GetComponent<WeaponController>();
-        movement = GetComponent<Movement>();
         if (unitData) {
             health = new FiniteField(unitData.health, unitData.healthRegen);
             stamina = new FiniteField(unitData.stamina, unitData.staminaRegen);
@@ -40,10 +37,7 @@ public class Unit : MonoBehaviour,IWidget
     }
 
     void Update() {
-        // Weapon input
-        if (weaponController != null && Input.GetMouseButton(0) && GameState.state == GameState.State.ingame) {
-            weaponController.Attack();
-        }
+
     }
 }
 
