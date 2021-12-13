60e78014f2746087f59f263653fa21061d6cea9e
Mohamed Elazab <66364977+elazabmohamed@users.noreply.github.com>
2021-12-13 20:07:02 +0300

    HUD and Scene Manager

---
+++ Warkey/Assets/Scripts/Menu/GameSceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public static GameSceneManager instance;
+
+    private void LoadGame(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+            yield return null;
+        }
+    }
+}
