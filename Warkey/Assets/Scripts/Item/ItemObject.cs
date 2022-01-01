ba8e7862a8103b60899ba838a0304fc84a42f09b
B-hzat <ardanuc.behzat@std.izu.edu.tr>
2022-01-01 17:10:53 +0300

    itemPickUp

--- Warkey/Assets/Scripts/Item/ItemObject.cs
+++ Warkey/Assets/Scripts/Item/ItemObject.cs
@@ -9,16 +9,37 @@ public class ItemObject : MonoBehaviour, IItem
     [SerializeField] private IItem.type type;
     [SerializeField] private float amount;
     [SerializeField] private float time;
+    PhotonView photonView;
 
     private bool isPicked;
 
     public bool IsPicked { get => isPicked; }
     public IItem.type Type { get => type; }
 
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    [PunRPC]
+    public void DestroyItem()
+    {
+        if (photonView.Owner == PhotonNetwork.LocalPlayer)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+   
     public ItemPicked PickUp()
     {
-        PhotonNetwork.Destroy(gameObject);
+        photonView.RPC(nameof(DestroyItem), RpcTarget.All);
         isPicked = true;
         return new ItemPicked(sprite,Type,amount,time);
+
     }
+
+
+
+    
+
 }
