f61bf5a7f101e75f27d25f87aa153d336d753869
TaBayram <48648542+TaBayram@users.noreply.github.com>
2022-01-02 18:43:49 +0300

    Camera

--- Warkey/Assets/Scripts/Entity/Movement/PlayerCamera.cs
+++ Warkey/Assets/Scripts/Entity/Movement/PlayerCamera.cs
@@ -8,6 +8,9 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField] private CinemachineFreeLook cinemachine;
     [SerializeField] private new Camera camera;
 
+    [SerializeField] private CinemachineOrbits[] orbits;
+    private int currentIndex = 0;
+
     bool hasBinded;
     Transform targetTransform;
 
@@ -25,5 +28,36 @@ public class PlayerCamera : MonoBehaviour
         if (hasBinded && ( targetTransform == null || targetTransform.gameObject == null)) {
             Destroy(this.gameObject);
         }
+        else {
+            if (Input.GetKeyDown(KeyCode.Mouse2)) {
+                currentIndex = (currentIndex + 1) % orbits.Length;
+                SetLook();
+            }
+
+        }
     }
+
+    private void SetLook() {
+        var orbit = orbits[currentIndex];
+        cinemachine.m_Orbits[0].m_Height = orbit.top.height;
+        cinemachine.m_Orbits[0].m_Radius = orbit.top.radius;
+        cinemachine.m_Orbits[1].m_Height = orbit.mid.height;
+        cinemachine.m_Orbits[1].m_Radius = orbit.mid.radius;
+        cinemachine.m_Orbits[2].m_Height = orbit.bot.height;
+        cinemachine.m_Orbits[2].m_Radius = orbit.bot.radius;
+    }
+}
+[System.Serializable]
+public struct CinemachineOrbits
+{
+    public Orbit top;
+    public Orbit mid;
+    public Orbit bot;
+}
+
+[System.Serializable]
+public struct Orbit
+{
+    public float height;
+    public float radius;
 }
